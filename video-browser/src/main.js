8746afcd2956eb82fc522331c077af5917cbcb56
Dawit Tafesse <dtafesse@uwm.edu>
2018-09-09 16:16:59 -0500

    start off images app

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
 
+Vue.config.productionTip = false;
+
 new Vue({
   // el: '#app', same as $mount('#app').. mounts App componet to the DOM
   render: h => h(App)
